a04286c5a826aeafa174a7be028d14806b5b6556
Wong Lok <wonglok@users.noreply.github.com>
2023-08-13 18:02:37 +0800

    fa

--- src/components/YoSphere.jsx
+++ src/components/YoSphere.jsx
@@ -1,19 +1,28 @@
+import { useSwan } from "../store/useSwan";
 import { Drei } from "../dx/LibraryCache";
 
-let { Sphere, MeshTransmissionMaterial } = Drei;
+let { Sphere, MeshTransmissionMaterial, Text3D, Center } = Drei;
 
 export function YoSphere() {
+  let baseURL = useSwan((r) => r.baseURL);
   return (
     <>
-      <Sphere>
-        <MeshTransmissionMaterial
-          metalness={0}
-          roughness={0.1}
-          transmission={1}
-          thickness={1.3}
-          color={"#ffffff"}
-        ></MeshTransmissionMaterial>
-      </Sphere>
+      <Center>
+        <Text3D
+          scale={2}
+          bevelEnabled={true}
+          font={`${baseURL}./fonts/days-font/Days_Regular.json`}
+        >
+          Hi
+          <MeshTransmissionMaterial
+            metalness={0}
+            roughness={0.1}
+            transmission={1}
+            thickness={1.3}
+            color={"#ffffff"}
+          ></MeshTransmissionMaterial>
+        </Text3D>
+      </Center>
     </>
   );
 }
