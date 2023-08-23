7cdd7c1b9bd35ec930b4a18fa5f23cb1810797ba
Wong Lok <wonglok@users.noreply.github.com>
2023-08-23 13:24:01 +0800

    fa

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    "eslint:recommended",
+    // "eslint:recommended",
     // "plugin:react/recommended",
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
@@ -11,10 +11,10 @@ module.exports = {
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   settings: { react: { version: "18.2" } },
   plugins: ["react-refresh"],
-  rules: {
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
-  },
+  // rules: {
+  //   "react-refresh/only-export-components": [
+  //     "warn",
+  //     { allowConstantExport: true },
+  //   ],
+  // },
 };
