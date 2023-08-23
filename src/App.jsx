10e5cc8d265bb546d7ac49bf73a0d5d37c4f6754
Wong Lok <wonglok@users.noreply.github.com>
2023-08-23 11:34:35 +0800

    fafa

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,28 @@
 // import viteLogo from "/vite.svg";
 import "./App.css";
 import { Canvas } from "@react-three/fiber";
-import { SwanRuntime } from "./Swan/SwanRuntime";
+// import { SwanRuntime } from "./Swan/SwanRuntime";
 import { Environment, OrbitControls } from "@react-three/drei";
+import { CommonSwanHTML, SwanLibRuntime } from "./Swan/SwanLibRuntime";
 
-let developmentURL = "http://localhost:8521";
-let productionURL = `https://swan-runtime.vercel.app`;
 function App() {
   return (
     <>
       <Canvas>
-        <SwanRuntime
+        {/* <SwanRuntime
           mode={"development"}
           developmentURL={developmentURL}
           productionURL={productionURL}
-        ></SwanRuntime>
+        ></SwanRuntime> */}
 
-        <Environment
-          files={`http://localhost:8521/hdr/grass.hdr`}
-          background
-        ></Environment>
+        <SwanLibRuntime></SwanLibRuntime>
+
+        <Environment files={`/hdr/grass.hdr`} background></Environment>
 
         <OrbitControls></OrbitControls>
       </Canvas>
+
+      <CommonSwanHTML></CommonSwanHTML>
     </>
   );
 }
