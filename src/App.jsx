008974d49e88b619e1420bcb9802a5dc98ca03a0
Wong Lok <wonglok@users.noreply.github.com>
2023-08-26 08:28:41 +0800

    app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@
 import { Canvas } from "@react-three/fiber";
 // import { SwanRuntime } from "./Swan/SwanRuntime";
 import { Environment, OrbitControls } from "@react-three/drei";
-import { LocalSwanHTML } from "./Swan/LocalSwanRuntime";
+import { LocalSwanHTML, LocalSwanRuntime } from "./Swan/LocalSwanRuntime";
 import { Suspense } from "react";
 import { RemoteSwanRuntime, RemoteSwanHTML } from "./Swan/RemoteSwanRuntime";
 
@@ -11,22 +11,23 @@ function App() {
   return (
     <>
       <Canvas>
-        {/* <LocalSwanRuntime
-          mode={"development"}
-          developmentURL={developmentURL}
-          productionURL={productionURL}
-        ></LocalSwanRuntime> */}
+        <group position={[0, 3, 0]}>
+          <Suspense fallback={null}>
+            <LocalSwanRuntime></LocalSwanRuntime>
+          </Suspense>
+        </group>
+
+        <group position={[0, -3, 0]}>
+          <Suspense fallback={null}>
+            <RemoteSwanRuntime
+              baseURL={`http://localhost:5174`}
+            ></RemoteSwanRuntime>
+            {/* <SwanLibRuntime></SwanLibRuntime> */}
+          </Suspense>
+        </group>
         <Suspense fallback={null}>
           <Environment files={`/hdr/grass.hdr`} background></Environment>
         </Suspense>
-
-        <Suspense fallback={null}>
-          <RemoteSwanRuntime
-            baseURL={`http://localhost:5174`}
-          ></RemoteSwanRuntime>
-          {/* <SwanLibRuntime></SwanLibRuntime> */}
-        </Suspense>
-
         <OrbitControls></OrbitControls>
       </Canvas>
 
